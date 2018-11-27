5df88faae8400ca3968fd30320272abc6b4b0a9b
Antony Chan <antony.chan@oocl.com>
2018-11-28 01:53:39 +0800

    feat: implement API of query companies by page and page size or get all

--- src/main/java/com/tw/apistackbase/controller/CompanyController.java
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -18,6 +18,15 @@ public class CompanyController {
         this.companyService = companyService;
     }
 
+    @GetMapping(produces = {"application/json"})
+    public ResponseEntity<List<Company>> query(@RequestParam(value = "page", required = false) Integer page,
+                                               @RequestParam(value = "pageSize", required = false) Integer pageSize){
+        if (page != null && pageSize != null){
+            return ResponseEntity.ok(companyService.findByPageAndPageSize(page, pageSize));
+        }
+        return ResponseEntity.ok(companyService.getAll());
+    }
+
     @PostMapping(produces = {"application/json"})
     public ResponseEntity<Company> addCompany(@RequestBody Company company) {
         return ResponseEntity.ok(companyService.add(company));
